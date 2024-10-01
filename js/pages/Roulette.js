89435e10a1f02b0aa3d928ab44dd31c24b22491c
alcra <65140768+alcragd@users.noreply.github.com>
2024-09-30 18:54:52 -0600

    AAAAA

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -185,12 +185,15 @@ export default {
                 return;
             }
         
-            const fullListMapped = fullList.map(([lvl, _], i) => ({
+            const fullListMapped = fullList
+            .filter(([lvl, err]) => lvl && !err)  // Filtrar niveles nulos o con errores
+            .map(([lvl], i) => ({
                 rank: i + 1,
                 id: lvl.id,
                 name: lvl.name,
                 video: lvl.verification,
             }));
+
         
             // Verificar si la lista se ha mapeado correctamente
             console.log("Mapped full list:", fullListMapped);
